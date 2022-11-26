168a5807f6f8fb59d7b8822d8ae976b005ca43ae
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2022-11-25 19:58:23 -0500

    Add date, title and rating to be sent to backend, reset data after submit/cancel

--- src/components/CreateNewEntry.vue
+++ src/components/CreateNewEntry.vue
@@ -6,17 +6,17 @@
       <v-menu v-model="displayDateMenu" :close-on-content-click="false" transition="scale-transition" offset-y
         max-width="35vmax" min-width="35vmax">
         <template v-slot:activator="{ on }">
-          <v-text-field color="#2f8f9d" label="Date" prepend-icon="mdi-calendar" readonly :value="getDisplayedDate"
+          <v-text-field color="#2f8f9d" label="Date" prepend-icon="mdi-calendar" readonly :value="displayedDate"
             v-on="on"></v-text-field>
         </template>
         <v-date-picker color="#2f8f9d" v-model="displayedDate" no-title width="35vmax"
           @input="displayDateMenu=false"></v-date-picker>
       </v-menu>
-      <v-text-field color="#2f8f9d" label="Title" autofocus></v-text-field>
+      <v-text-field color="#2f8f9d" v-model="newTitle" label="Title" autofocus></v-text-field>
       <v-textarea color="#2f8f9d" v-model="newEntry" label="Dear diary..."></v-textarea>
       <h3>Rate your day:</h3>
       <v-rating empty-icon="mdi-heart-outline" full-icon="mdi-heart" hover length="5" size="50" color="#ffd1dc"
-        background-color="#ffd1dc"></v-rating>
+        background-color="#ffd1dc" v-model="newRating"></v-rating>
       <v-btn id="submit-btn" rounded small @click="addEntry">Submit</v-btn>
       <v-btn id="delete-btn" rounded small @click="cancelAddEntry">Cancel</v-btn>
     </v-form>
@@ -31,7 +31,8 @@ export default {
       newEntry: '',
       showTextField: false,
       displayDateMenu: false,
-      displayedDate: null,
+      displayedDate: this.getCurrentDate(), 
+      newRating: 0
     }
   },
   props: ['entries'],
@@ -39,24 +40,32 @@ export default {
     async addEntry() {
       this.showTextField = false
       const newEntryObject = {
-        content: this.$data.newEntry
+        date: this.$data.displayedDate,
+        title: this.$data.newTitle,
+        content: this.$data.newEntry,
+        rating: this.$data.newRating
       }
+      console.log(newEntryObject)
       const response = await this.$http.post('http://localhost:8000/api/journal_entries/', newEntryObject);
       this.$props.entries.push(response.data)
       console.log(this.$props.entries)
       this.$data.newEntry = ''
-      this.$data.displayedDate = null
+      this.$data.displayedDate = this.getCurrentDate()
+      this.$data.newTitle = ''
+      this.$data.newRating = 0
     },
     cancelAddEntry() {
       this.$data.newEntry = ''
       this.showTextField = false
-      this.$data.displayedDate = null
-    }
-  },
-  computed: {
-    getDisplayedDate() {
-      return this.displayedDate;
+      this.$data.displayedDate = this.getCurrentDate()
+      this.$data.newTitle = ''
+      this.$data.newRating = 0
     },
+    getCurrentDate() {
+      const dateObj = new Date();
+      const newDate = dateObj.getUTCFullYear() + "-" + (dateObj.getUTCMonth() + 1) + "-" + dateObj.getUTCDate();
+      return newDate;
+    }
   },
 }
 </script>
